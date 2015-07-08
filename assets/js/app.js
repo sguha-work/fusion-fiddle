25c913f13fd99a01668e05809597d23f86773114
sguha-fc <sahasrangshu@fusioncharts.com>
2015-07-08 14:20:39 +0530

    changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,23 @@
 var getHTMLContent = (function() {
-	
+	var htmlContent = $("#div_html textarea").val();
+	return htmlContent
 });
+
+var getCSSContent = (function() {
+	var cssContent = $("#div_css textarea").val();
+	return cssContent
+});
+
+var getJSContent = (function() {
+	var jsContent = $("#div_js textarea").val();
+	return jsContent
+});
+
 $(document).ready(function() {
 	$("button").on('click', function() {
 		var htmlContent = getHTMLContent();
+		var cssContent = getCSSContent();
+		var jsContent = getJSContent();
+		var selectedLibrary = $("#sel_library").val();
 	})
 });
