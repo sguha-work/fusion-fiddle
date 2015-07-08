47cf16fda5cc2ddabcb5fe462f2f73250a1ac25d
sguha-fc <sahasrangshu@fusioncharts.com>
2015-07-08 15:48:21 +0530

    basic done

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,11 +13,37 @@ var getJSContent = (function() {
 	return jsContent
 });
 
+var prepareFullHTML = (function(htmlContent, cssContent, jsContent, selectedLibrary) {
+	var finalHTMLContent = '<!DOCTYPE html><html><head>';
+	
+	// adding the library
+	if($.trim(selectedLibrary)) {
+		finalHTMLContent += '<script type="text/javascript" src="'+selectedLibrary+'"></script>';
+	}
+
+	// adding the css
+	finalHTMLContent += "<style>"+cssContent+"</style>";
+
+	finalHTMLContent += "</head><body>";
+
+	// adding html content
+	finalHTMLContent += htmlContent;
+
+	// adding the javascript
+	finalHTMLContent += "<script type='text/javascript'>"+jsContent+"</script>";
+
+	finalHTMLContent += "</body></html>";
+
+	return finalHTMLContent;
+});
+
 $(document).ready(function() {
 	$("button").on('click', function() {
 		var htmlContent = getHTMLContent();
 		var cssContent = getCSSContent();
 		var jsContent = getJSContent();
 		var selectedLibrary = $("#sel_library").val();
-	})
+		var finalHTMLContent = prepareFullHTML(htmlContent, cssContent, jsContent, selectedLibrary);
+		$('#div_output iframe').attr('src',"data:text/html;charset=utf-8," + escape(finalHTMLContent));
+	});
 });
